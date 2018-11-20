3dca97af16902d04f18ff7cc41bd0835a5d09e2b
Justin Huynh <polar.truth@gmail.com>
2018-11-20 12:04:50 -0500

    Added insertion and removal of trains

--- src/parksSimModel/TrainLine.java
+++ src/parksSimModel/TrainLine.java
@@ -1,7 +1,26 @@
 package parksSimModel;
+import java.util.*;
 
 class TrainLine {
-
+	int n; //
+	private ArrayList<Train> trainList = new ArrayList<Train>(); 
+	
+	protected int getN() { 
+		return(trainList.size());
+	}
+	
+	protected void spInsertQue(Train train) {
+		trainList.add(train);
+	}
+	
+	protected Train spRemoveQue() {
+		Train train = null;
+		if (trainList.size() != 0){
+			train = trainList.remove(0);
+		}
+		return train;
+	}
+	
 	//Attributes
 	//n and list no explicit attributes. See examples, e.g. KK Counter or Port Berths
 }
